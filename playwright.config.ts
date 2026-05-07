8514652a487df714f2a7b88013f6d524f98b2529
thaonguyen20312 <thaonguyen20312@gmail.com>
2026-05-07 17:42:01 +0700

    edit config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,10 +31,12 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'data-test',
-    headless: false, // show browser
+    headless: process.env.CI ? true : false, // show browser
     launchOptions: {
-      slowMo: 2000
+      slowMo: process.env.CI ? 0 : 2000
     },
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
   /* Configure projects for major browsers */
   projects: [
